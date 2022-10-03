917ff7810fe733a2f60284e61021c7cce8137d87
Hackids32 <inside.man108@gmail.com>
2022-10-03 10:01:59 +0700

    Initial Commit

---
+++ Latihan_DataModel/Latihan_DataModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Latihan_DataModelApp.swift
+//  Latihan_DataModel
+//
+//  Created by Hackids on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Latihan_DataModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
